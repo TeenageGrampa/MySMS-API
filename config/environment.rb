7c0c7ba3a286dbf54c1d2bb28900d0c99992b655
TeenageGrampa <danielhvoigt@gmail.com>
2025-08-22 14:27:31 -0400

    changing envs for deploy

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the Rails application.
 require_relative "application"
 
-app_environment_variables = File.join(Rails.root, 'config', 'app_environment_variables.rb')
-load(app_environment_variables)
-
 # Initialize the Rails application.
 Rails.application.initialize!
