26e4fcdbf1a71017f6b31aade58de23e75f9e97b
TeenageGrampa <danielhvoigt@gmail.com>
2025-08-22 16:00:37 -0400

    adding approved hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,4 +74,6 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts << /.*\.surge\.sh/
+  config.hosts << /.*\.herokuapp\.com/
 end
